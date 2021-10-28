21f01713627c2e01c27563bf739e4606f49f73d7
pavelchowdhury99 <59216690+pavelchowdhury99@users.noreply.github.com>
2021-10-28 09:02:15 +0530

    Updating logging levels, adding proxy rotator and test

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -3,6 +3,7 @@ import os
 import logging
 import random
 import functools
+import time
 from collections import defaultdict
 import pandas as pd
 import requests
@@ -17,30 +18,31 @@ class Proxy:
     ''' Class to store data related to proxies'''
 
     # Class variable defined here
-    with open(Path(os.path.join(os.path.dirname(__file__),'user_agents.json')), 'r') as f:
+    with open(Path(os.path.join(os.path.dirname(__file__), 'user_agents.json')), 'r') as f:
         __user_agent_list = json.load(f).get("user_agents")
         logger.debug(f"Loaded list of user agents from json file")
         logger.debug(f"First and last are - {__user_agent_list[0]} and {__user_agent_list[-1]}")
 
     @functools.lru_cache
     def __init__(self):
-        with open(Path(os.path.join(os.path.dirname(__file__),'proxy.json')), 'r') as f:
+        with open(Path(os.path.join(os.path.dirname(__file__), 'proxy.json')), 'r') as f:
             self.__proxy = json.load(f)
-            logger.info(f"Loaded proxy {self.__proxy} from json file")
+            logger.debug(f"Loaded proxy {self.__proxy} from json file")
 
     @property
     def user_agent(self) -> dict:
         """Provides a random user agent from list of user agents"""
+        logger.debug("Fetching a random user agent...")
         return {"User-Agent": random.choice(self.__user_agent_list)}
 
     @classmethod
     def get_random_user_agent(cls):
         """Provides a random user agent from list of user agents"""
+        logger.debug("Fetching a random user agent...")
         return {"User-Agent": random.choice(cls.__user_agent_list)}
 
     @staticmethod
-    def get_new_proxy_for_url(url: str, verify: bool = True, timeout: int = 200,
-                              proxy_json_path: str = Path(r"proxy\proxy.json")) -> dict:
+    def get_new_proxy_for_url(url: str, verify: bool = True, timeout: int = 200) -> dict:
         """Getting new proxy for the given url"""
 
         proxy_website_url = 'https://www.us-proxy.org/'
@@ -52,9 +54,11 @@ class Proxy:
         # checking if the url which needs proxy is secured or not
         # and filtering the proxy list based on that
         if "https" in url:
+            logger.debug("Fetching new proxy for https type url..")
             proxy_df = proxy_df[0][proxy_df[0]['Https'] == 'yes']
             key = "https"
         else:
+            logger.debug("Fetching new proxy for http type url..")
             proxy_df = proxy_df[0][proxy_df[0]['Https'] == 'no']
             key = "https"
 
@@ -68,23 +72,27 @@ class Proxy:
                     logging.info(f"Using proxy {proxy_dict}")
                     return proxy_dict
             except Exception as e:
-                logging.info(
+                logging.debug(
                     f"Proxy {proxy_dict} {count} failed because of {e}")
                 continue
-        raise Exception("No Proxy Found !")
+        raise Exception("No Proxy Found!")
 
     def get_working_proxy_for_url(self, url: str, verify: bool = True, timeout: int = 200,
-                                  proxy_json_path: str = os.path.join(os.path.dirname(__file__),'proxy.json')) -> dict:
+                                  proxy_json_path: str = os.path.join(os.path.dirname(__file__),
+                                                                      'proxy.json')) -> dict:
         try:
             if requests.get(url, proxies=self.__proxy).ok:
                 logger.info(f"Using existing proxies {self.__proxy}")
                 return self.__proxy
             else:
-                new_proxy_dict = Proxy.get_new_proxy_for_url(url=url, verify=True)
+                new_proxy_dict = Proxy.get_new_proxy_for_url(url=url,
+                                                             verify=verify,
+                                                             timeout=timeout)
                 self.__proxy.update(new_proxy_dict)
                 return self.__proxy
         except requests.exceptions.ProxyError:
-            new_proxy_dict = Proxy.get_new_proxy_for_url(url=url, verify=True)
+            new_proxy_dict = Proxy.get_new_proxy_for_url(url=url,
+                                                         verify=True)
             self.__proxy.update(new_proxy_dict)
             return self.__proxy
         except Exception as e:
@@ -94,28 +102,28 @@ class Proxy:
                 json.dump(self.__proxy, f)
                 logger.info(f"Wrote new proxy {self.__proxy} to {proxy_json_path}")
 
-    @staticmethod
-    def get_proxy_by_location(location: str) -> dict:
-        # using default dict so that if someone passes wrong country code it will not through KeyError
-        dict_of_proxies_by_location = defaultdict(lambda x: None)
-        # use the following site for proxies
-        # http://free-proxy.cz/en/proxylist/country/US/https/ping/all
-        dict_of_proxies_by_location.update(dict(us={
-            "http": "http://132.226.36.165:3128",
-            "https": "https://104.129.196.54:8800",
-            "ftp": "ftp://10.10.1.10:3128"
-        }))
-        # if correct, provides location proxy dict else None
-        if location:
-            return dict_of_proxies_by_location[location]
-        # if location is None returns None
-        return dict()
+    # @staticmethod
+    # def get_proxy_by_location(location: str) -> dict:
+    #     # using default dict so that if someone passes wrong country code it will not through KeyError
+    #     dict_of_proxies_by_location = defaultdict(lambda x: None)
+    #     # use the following site for proxies
+    #     # http://free-proxy.cz/en/proxylist/country/US/https/ping/all
+    #     dict_of_proxies_by_location.update(dict(us={
+    #         "http": "http://132.226.36.165:3128",
+    #         "https": "https://104.129.196.54:8800",
+    #         "ftp": "ftp://10.10.1.10:3128"
+    #     }))
+    #     # if correct, provides location proxy dict else None
+    #     if location:
+    #         return dict_of_proxies_by_location[location]
+    #     # if location is None returns None
+    #     return dict()
 
     @staticmethod
     def get_proxy_rotator() -> cycle:
         proxy_url = 'https://free-proxy-list.net/'
-        df = pd.read_html(requests.get(proxy_url, headers={
-            'User-Agent': Proxy.get_random_ua()}).text)[0]
+        df = pd.read_html(requests.get(proxy_url,
+                                       headers=Proxy.get_random_user_agent()).text)[0]
         df = df[df['Https'] == 'yes']
         df['http'] = df[['IP Address', 'Port']].apply(
             lambda x: f'http://{x[0]}:{x[1]}', axis=1)
@@ -128,22 +136,33 @@ class Proxy:
 @log_durations(logging.info)
 def main():
     proxy = Proxy()
-    print(proxy.user_agent)
-    x = proxy.get_working_proxy_for_url(url="https://www.mcallen.net/departments/bridge/anzalduas")
-    requests.get("https://www.mcallen.net/departments/bridge/anzalduas", proxies=x)
-    print("Done!!!!")
+    logger.info(proxy.user_agent)
+    x = proxy.get_working_proxy_for_url(url="https://www.netrma.org/net-rma-policies/toll-rates/", verify=False)
+    logger.info("sleeping for 2 secs...")
+    time.sleep(2)
+    logger.info("Just woke up!")
+    requests.get("https://www.netrma.org/net-rma-policies/toll-rates/", proxies=x, verify=False)
+    logger.info("Done!!!!")
 
 
-# # Test urls
-# # "https://www.mcallen.net/departments/bridge/mcallen-hidalgo"
-# # "https://www.mcallen.net/departments/bridge/anzalduas"
-# # "https://www.netrma.org/net-rma-policies/toll-rates/"
-# # "https://www.netrma.org/net-rma-policies/toll-rates/"
+@log_durations(logging.info)
+def test():
+    cycle_obj = Proxy.get_proxy_rotator()
+    for idx, proxy in enumerate(cycle_obj):
+        if idx > 4:
+            break
+        print(proxy)
 
-# # References
-# # https://stackoverflow.com/questions/66642705/why-requests-raise-this-exception-check-hostname-requires-server-hostname
+
+# Test urls
+# "https://www.mcallen.net/departments/bridge/mcallen-hidalgo"
+# "https://www.mcallen.net/departments/bridge/anzalduas"
+# "https://www.netrma.org/net-rma-policies/toll-rates/"
+
+# References
+# https://stackoverflow.com/questions/66642705/why-requests-raise-this-exception-check-hostname-requires-server-hostname
 
 if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                        datefmt='%H:%M:%S', level=logging.INFO)
-    main()
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        datefmt='%H:%M:%S', level=logging.DEBUG)
+    test()
--- proxy/test_proxy.py
+++ proxy/test_proxy.py
@@ -4,18 +4,27 @@ from proxy import Proxy
 
 logger = logging.getLogger(__name__)
 
-## test 1
+
+# test 1
+# test loading class attribute __user_agent
+# ,__init__ and loading from proxy.json
 @log_durations(logging.info)
 def test_property_method_get_random_ua():
-    proxy=Proxy()
+    proxy = Proxy()
     print(proxy.user_agent)
 
-## test 2 
+
+# test 2
+# test get_random_user_agent class method
+# prints a dict
 @log_durations(logging.info)
 def test_class_method_get_random_ua():
     print(Proxy.get_random_user_agent())
 
-## test 3
+
+# test 3
+# test static get_new_proxy_for_url
+# and prints a dict
 @log_durations(logging.info)
 def test_static_method_get_new_proxy_for_url():
     # # Test urls
@@ -25,16 +34,30 @@ def test_static_method_get_new_proxy_for_url():
     # # "https://www.netrma.org/net-rma-policies/toll-rates/"
     print(Proxy.get_new_proxy_for_url(url="https://www.mcallen.net/departments/bridge/mcallen-hidalgo"))
 
-## test 4
+
+# test 4
+# test method get_working_proxy_for_url
+# and prints a dict and updates the proxy.json
 @log_durations(logging.info)
 def test_getting_new_working_proxies_and_saving_to_file():
-    proxy=Proxy()
+    proxy = Proxy()
     print(proxy.get_working_proxy_for_url(url="https://www.netrma.org/net-rma-policies/toll-rates/"))
 
-if __name__ == "__main__":
 
+# test 5
+# check get_proxy_rotator static method
+def test_check_get_proxy_rotator():
+    for idx, proxy in enumerate(Proxy.get_proxy_rotator()):
+        if idx == 4:
+            break
+        assert type(proxy) == dict
+        assert proxy.get('https') is not None
+    logger.debug("Done checking get_proxy_rotator")
+
+
+if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        datefmt='%H:%M:%S', level=logging.INFO)
+                        datefmt='%H:%M:%S', level=logging.DEBUG)
 
     # test 1
     test_property_method_get_random_ua()
@@ -47,3 +70,6 @@ if __name__ == "__main__":
 
     # test 4
     test_getting_new_working_proxies_and_saving_to_file()
+
+    # test 5
+    test_check_get_proxy_rotator()
